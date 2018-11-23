52f8e48a94098acf41c2dad329d6309a2e806a60
doug rudolph <drudolph914@gmail.com>
2018-11-22 22:17:05 -0500

    typing is working

--- static/js/type.js
+++ static/js/type.js
@@ -1,28 +1,44 @@
-function main(){
-    setInterval(caretToggle, 500);
+class Sequence{
 
-    function caretToggle() {
-        let caret = document.getElementsByClassName("term-caret")[0];
-        if (caret.classList.contains('blink'))
-            caret.classList.remove('blink');
-        else
-            caret.classList.add('blink');
+    constructor(text, speed, color, elementId){
+        this.text = text;
+        this.speed = speed;
+        this.color = color;
+        this.elementId = elementId;
+
+        // keeps track of how much text has been written on screen
+        this.writeLen = 0;
     }
-
-    let term_inp = document.getElementsByClassName('term-input-hide')[0]
-    let term_cmd = document.getElementsByClassName('term-cmd')[0];
-    term_inp.addEventListener("keyup", typeCommand);
-
-    function typeCommand(e) {
-      console.log(e)
-      let v = term_inp.value;
-      term_cmd.innerHTML = v;
-    }
-
-    //let term = document.getElementById('term');
-    //term.addEventListener("click", term_inp.focus())
 }
 
-window.onload = function(){
-    main();
+
+class TypeWriter{
+
+    constructor(sequences){
+        this.sequences = sequences;
+    }
+
+	write(){
+	    if(this.sequences == null || this.sequences.length == 0)
+	        return;
+
+        function writeSequence(sequence){
+            if (sequence.writeLen < sequence.text.length){
+                let elmnt = document.getElementById("test");
+                elmnt.innerHTML += sequence.text.charAt(sequence.writeLen);
+                sequence.writeLen++;
+                setTimeout(function(){writeSequence(sequence);}, sequence.speed);
+            }
+        }
+
+        let cur_seq = this.sequences[0];
+        writeSequence(cur_seq);
+    }
 }
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    let seq = new Sequence('Diana smells like poop, Diana smells like poop', 100, null);
+    let writer = new TypeWriter([seq]);
+    writer.write();
+});
