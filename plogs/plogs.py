144a40551f4c491b37753827517be633b0cdbd1b
doug rudolph <drudolph914@gmail.com>
2018-12-04 12:57:05 -0500

    going to remove comments until code is cleaned up

--- plogs/plogs.py
+++ plogs/plogs.py
@@ -1,42 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-""" plogs main python module
-
-File: plogs.py
-Author: Doug Rudolph
-Created: November 19, 2018
-
-
-Goals:
-    Pretty Logs is a python module that is meant to color code program logs and log files. The main goals
-    as a package are:
-
-    1.) 6 easy to understand logging level colors that will color code all of the logs.
-        - debug:    Color varies, either no color, or colorcoded objec
-        - info:     White
-        - status:   Bold Gray
-        - warning:  Orange
-        - error:    Red
-        - critical: Bold Red
-
-    2.) Debugging tools that color code built in python objects: Dictionary and List objects.
-    3.) A general set of formatting tools that makes coloring logs intuitive and easy to read.
-
-"""
-
 from logutils import LogLevel, Colors, Levels
 
 import pprint
 import sys
+
+
 
 # global reference to logger (this is because _Logger is a singleton)
 _LOGGER_REF = None
 
 
 def get_logger():
-    """ Returns logger to to user """
-
     global _LOGGER_REF
 
     if _LOGGER_REF is None:
@@ -46,28 +22,6 @@ def get_logger():
 
 
 class _Logger:
-    """ Logger class description
-
-    Logger is the main object that gets interfaced with by the user. Logger as an object accepts all
-    the settings that formats the logs, writes the log text, and colorcodes text,
-
-    Config Settings:
-        There are two main ways to use Pretty Logs
-
-        1.) The first option is to send in an already existing logger, and Pretty Logs will map the default
-            logging levels to the proper colors.
-            - NOTE*: when doing this a lot of the logging customization is lost, but in return, the original
-                     logging settings/formatting is kept, and colors are still applied to your logs.
-
-        2.) The other way to config Pretty Logs is to allow Pretty logs to handle all the general logging settings
-            as well as log formatting. By doing this, just be aware that soem formating tools will be not be
-            accessible.
-
-    TODO:
-        * Add tests
-        * Add tables
-        * Add default logging wrapper
-    """
 
     def __init__(self):
         # instances of Color and levels enum
@@ -95,21 +49,13 @@ class _Logger:
         self._pretty = pretty
         self._show_levels = show_levels
         self._show_time = show_time
-        self._to_file = True
+        self._to_file = to_file
         self._file_location = file_location
 
     def format(self, fstr):
-        """ Customizes the output of the log string """
         self.fstr = fstr
 
     def bind(self, logger):
-        """ The purpose of this binding function is to be able to use
-        the default logger that's built into python, along side Pretty
-        Logs
-
-        Args:
-            logger (logging): Instance of built in python logger
-        """
         # self.logger = logger
         pass
 
