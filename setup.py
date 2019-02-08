0525be7eac114b4a82a1203d34f563d9ea24571c
11 <drudolph914@gmail.com>
2019-02-08 13:03:22 -0500

    updating setup.py file, and moving all source code to src dir

--- setup.py
+++ setup.py
@@ -1,20 +1,35 @@
 import setuptools
 
-
+# copies README into `long_description`
 with open('README.md', 'r') as readme:
     long_description = readme.read()
 
 
 setuptools.setup(
+
+    # general metadata
     name="plogs",
-    version="0.0.1",
+    version="0.1.1",
     author="Doug Rudolph",
     author_email="drudolph914@gmail.com",
+
+    # shorthand description
     description="Bring color and formatting to your logs",
+
+    # copies the README.md into `long_description` and notes that it is a markdown file
     long_description=long_description,
     long_description_content_type="text/markdown",
+
+    # Github URL
     url="https://github.com/11/plogs",
+
+    # takes note that the python modules are contained in the source folder
+    package_dir={'': 'src:'},
+
+    # setuptools will find the python packages for us
     packages=setuptools.find_packages(),
+
+    # specify what version of python plogs is targeting
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
