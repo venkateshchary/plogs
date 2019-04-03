f201da8607f37ac66647b0b1a35ceff12c05fc70
11 <drudolph914@gmail.com>
2019-04-02 21:50:40 -0400

    got all the basic data from the traceback

--- src/errorutils.py
+++ src/errorutils.py
@@ -16,7 +16,6 @@ def _functrace(func, *args, **kwargs):
         >>>     error = 1/0
         >>>
         >>> # Results in formatted stack trace
-        >>>
 
     Args:
         func (function): decorated function
@@ -27,9 +26,7 @@ def _functrace(func, *args, **kwargs):
         *print the exception name
         *get the module name in the output
         *actually color code output
-
     """
-
     try:
         # try running decorated function and see if an error occurs
         result = func(*args, **kwargs)
@@ -42,18 +39,24 @@ def _functrace(func, *args, **kwargs):
         traceback = traceback.tb_next
 
         while traceback:
-            # get the stack frame, filename, line number of error
+            # get the stack frame, filename, and line number of error
             frame = traceback.tb_frame
             filename = traceback.tb_frame.f_code.co_filename
             lineno = traceback.tb_lineno
 
             # Step into the line cache to get the source code of the error
             linecache.checkcache(filename)
-            line_src = linecache.getline(filename, lineno, frame.f_globals)
+            src_code = linecache.getline(filename, lineno, frame.f_globals)
 
             # generate and print error message
-            err_msg = f'File "{filename}", line {lineno}, ...\n{line_src}'
+            err_msg = f'File "{filename}", line {lineno}, ...\n{src_code}'
             print(err_msg)
 
             # go to next stack frame
             traceback = traceback.tb_next
+
+        # log final error message
+        exception = str(err.__class__.__name__)
+        err_msg = str(err)
+        err_msg = f'{exception}: {err_msg}'
+        print(err_msg)
